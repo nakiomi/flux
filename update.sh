343d29d52a5e7fd1aae2c9ee2770e12e0bf7b077
fauzi277 <46618042+fauzi277@users.noreply.github.com>
2024-01-18 23:52:31 +0800

    back to old

--- update.sh
+++ update.sh
@@ -1,39 +1,20 @@
+jelaskan kode ini
+
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 echo -e "${GREEN}Updating proxy...${ENDCOLOR}"
-
-# Check if "flux" file exists
+clear
+sleep 1
 if [ -f "flux" ]; then
     echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
     rm flux
-    if [ $? -ne 0 ]; then
-        echo -e "${RED}Error deleting old proxy.${ENDCOLOR}"
-        exit 1
-    fi
     sleep 1
 fi
-
-# Check and download new proxy only if the version is different
-current_version=$(./flux --version)  # Change this to get the version from the existing flux file
-latest_version=$(wget -qO- https://github.com/Zweaf/flux/raw/main/flux --version)  # Change this to get the version from the online source
-
-if [[ "$current_version" != "$latest_version" ]]; then
-    echo -e "${GREEN}Getting new proxy...${ENDCOLOR}"
-    wget -q https://github.com/Zweaf/flux/raw/main/flux
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}Download successful.${ENDCOLOR}"
-    else
-        echo -e "${RED}Download failed.${ENDCOLOR}"
-        exit 1
-    fi
-    sleep 1
-else
-    echo -e "${GREEN}Proxy is already up to date.${ENDCOLOR}"
-fi
-
-# Execute the updated proxy
+echo -e "${GREEN}Getting new proxy...${ENDCOLOR}"
+wget -q https://github.com/Zweaf/flux/raw/main/flux
+sleep 1
 echo -e "${GREEN}Executing proxy...${ENDCOLOR}"
 chmod +x flux
 ./flux
